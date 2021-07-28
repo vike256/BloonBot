6b6f8bb8bfd3748f70002b337210fca614965ab5
Eli Dacreach <elitecreature@gmail.com>
2021-07-28 02:15:31 -0500

    Slimed times for 15-27. Crash after 27.

--- dark_castle_Impoppable.py
+++ dark_castle_Impoppable.py
@@ -438,7 +438,7 @@ def Round_100():
 
 def Main_Game():
 
-    sleep(2)
+    sleep(4)
 
     print(f'{Fore.YELLOW}Prepping Field.')
     click("EVENT_EXIT")
@@ -451,33 +451,33 @@ def Main_Game():
     Null_Round(10) # Round 6
     Null_Round(12) # Round 7
     place_tower("HERO", "HERO_LOCATION")
-    Null_Round(14) # Round 8
-    Null_Round(16) # Round 9
-    Null_Round(18) # Round 10
+    Null_Round(12) # Round 8
+    Null_Round(14) # Round 9
+    Null_Round(15) # Round 10
     upgrade_tower('3', "SUBMARINE_1_LOCATION") # Sub_1 001
     upgrade_tower('1', "SUBMARINE_1_LOCATION") # Sub_1 101
-    Null_Round(20) # Round 11
-    Null_Round(20) # Round 12
-    Null_Round(20) # Round 13
+    Null_Round(15) # Round 11
+    Null_Round(15) # Round 12
+    Null_Round(15) # Round 13
     upgrade_tower('1', "SUBMARINE_1_LOCATION") # Sub_1 201
-    Null_Round(20) # Round 14
-    Null_Round(20) # Round 15
-    Null_Round(20) # Round 16
-    Null_Round(20) # Round 17
-    Null_Round(20) # Round 18
+    Null_Round(15) # Round 14
+    Null_Round(12) # Round 15
+    Null_Round(10) # Round 16
+    Null_Round(7) # Round 17
+    Null_Round(15) # Round 18
     upgrade_tower('3', "SUBMARINE_1_LOCATION") # Sub_1 202
-    Null_Round(20) # Round 19
+    Null_Round(11) # Round 19
     upgrade_tower('3', "DART_MONKEY_LOCATION") # Dart Monkey 001
     upgrade_tower('3', "DART_MONKEY_LOCATION") # Dart Monkey 002
-    Null_Round(20) # Round 20
-    Null_Round(20) # Round 21
-    Null_Round(20) # Round 22
-    Null_Round(20) # Round 23
-    Null_Round(20) # Round 24
+    Null_Round(9) # Round 20
+    Null_Round(13) # Round 21
+    Null_Round(10) # Round 22
+    Null_Round(10) # Round 23
+    Null_Round(10) # Round 24
     upgrade_tower('3', "SUBMARINE_1_LOCATION") # Sub_1 203
-    Null_Round(20) # Round 25
-    Null_Round(20) # Round 26
-    Null_Round(20) # Round 27
+    Null_Round(14) # Round 25
+    Null_Round(12) # Round 26
+    Null_Round(16) # Round 27
     place_tower("BANANA", "BANANA_FARM_1_LOCATION") 
     Null_Farming_Round(20, BANANA_FARM_1_LOCATION) # Round 28
     Null_Farming_Round(20, BANANA_FARM_1_LOCATION) # Round 29
